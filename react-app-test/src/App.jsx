cf69e92e7b4e4b540cfdbef8fc8cddff6039a076
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-21 21:16:17 +0530

    adding the useeffect

--- react-app-test/src/App.jsx
+++ react-app-test/src/App.jsx
@@ -18,4 +18,32 @@
 //   );
 // }
 
+// export default App;
+// useeffect
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [exchangeData, setExchangeData] = useState({});
+  const [bankData, setBankData] = useState({});
+
+  useEffect(() => {
+    setTimeout(() => {
+      setExchangeData({
+        returns: 100,
+      });
+    }, 1000);
+  });
+
+  fetch("https://google.com", async (res) => {
+    const json = await res.json();
+    setBankData(json);
+    // Assume it is { income: 100 }
+  });
+
+  const incomeTax = (bankData.income + exchangeData) * 0.3;
+
+  return <div>hi there, your income tax returns are {incomeTax}</div>;
+}
+
 export default App;
