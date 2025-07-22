3866c6c6e000a8ed72f8a617ec9988568f16e4a5
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-22 07:45:18 +0530

    adding use memo

--- react-app-test/src/App.jsx
+++ react-app-test/src/App.jsx
@@ -21,27 +21,68 @@
 // export default App;
 // useeffect
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [exchangeData, setExchangeData] = useState({});
+//   const [bankData, setBankData] = useState({});
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setExchangeData({
+//         returns: 100,
+//       });
+//     }, 1000);
+//   });
+
+//   fetch("https://google.com", async (res) => {
+//     const json = await res.json();
+//     setBankData(json);
+//     // Assume it is { income: 100 }
+//   });
+
+//   const incomeTax = (bankData.income + exchangeData) * 0.3;
+
+//   return <div>hi there, your income tax returns are {incomeTax}</div>;
+// }
+
+// export default App;
+
+// use memo
+import { useEffect, useState, useMemo } from "react";
 
 function App() {
-  const [exchangeData, setExchangeData] = useState({});
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
   const [bankData, setBankData] = useState({});
 
   useEffect(() => {
+    // Some operation to get the data
+    setExchange1Data({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    // Some operation to get the data
+    setExchange2Data({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    // Some operation to get the data
     setTimeout(() => {
-      setExchangeData({
-        returns: 100,
+      setBankData({
+        income: 100,
       });
-    }, 1000);
-  });
+    });
+  }, []);
 
-  fetch("https://google.com", async (res) => {
-    const json = await res.json();
-    setBankData(json);
-    // Assume it is { income: 100 }
-  });
-
-  const incomeTax = (bankData.income + exchangeData) * 0.3;
+  const cryptoReturns = useMemo(() => {
+    exchange1Data.returns + exchange2Data.returns;
+  }, [exchange1Data, exchange2Data]);
+  const incomeTax = (cryptoReturns + bankData.income) * 0.3;
 
   return <div>hi there, your income tax returns are {incomeTax}</div>;
 }
